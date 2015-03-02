d3f2f33a960ae7c34019f34904375cb6a1da67e1
justplus <zhaoliang@iflytek.com>
2015-03-02 18:52:01 +0800

    first commit, tab added

---
+++ WeiboIOS/WeiboIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiboIOS
+//
+//  Created by zhaoliang on 15/3/1.
+//  Copyright (c) 2015年 zhaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
