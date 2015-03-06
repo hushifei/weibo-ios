4223b2d8ed8e2db11ffd06735f8c5cb2e087c5e8
justplus <zhaoliang@iflytek.com>
2015-03-06 21:41:39 +0800

    stop to use ib, code layout instead

---
+++ konggu/konggu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  konggu
+//
+//  Created by zhaoliang on 15/3/6.
+//  Copyright (c) 2015年 zhaoliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
